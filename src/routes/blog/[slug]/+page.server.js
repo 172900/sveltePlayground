f0705e65de6ca4ce7f355e46a5c51c90d32393ee
172900 <1729175@gmail.com>
2023-08-02 08:26:46 +0900

    서버사이드 랜더링 + 라우팅 파일 기반 에러 페이지 출력

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -2,11 +2,12 @@ import { error } from '@sveltejs/kit';
 
 export function load({ params }) {
     if (params.slug === 'hello-world') {
+        console.log('server!!!')
         return {
             title: 'Hello world!',
             content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
         };
     }
 
-    throw error(404, 'Not found');
+    throw error(404, 'Not Good');
 }
